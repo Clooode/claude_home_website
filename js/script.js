299f002d7d4af7af6d109264810a6ac3edf96d4a
Clooode <sigourneycox@gmail.com>
2025-06-21 22:56:32 +1000

    Initial commit
    Rough draft of landing page, baking page, and about me

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.getElementById('crochet').addEventListener('click', function() {
+  window.location.href = 'crochet.html';
+});
+
+document.getElementById('birds').addEventListener('click', function() {
+  window.location.href = 'birds.html';
+});
+
+document.getElementById('baking').addEventListener('click', function() {
+  window.location.href = 'baking.html';
+});
+
+
+document.getElementById('work').addEventListener('click', function() {
+  window.location.href = 'work.html';
+});
+
+
+
+document.getElementById('snoot').addEventListener('click', function() {
+  window.location.href = 'bears.html';
+});
